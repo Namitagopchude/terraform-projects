b34c0f4be0973a16dd05a2e7f76de8dc618d8221
unknown <gopchude.namita@gmail.com>
2023-08-29 17:53:33 +0530

    created new changes

---
+++ cicd/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# fail on any error
+set -eu
+
+# install yum-config-manager to manage your repositories
+sudo yum install -y yum-utils
+
+# use yum-config-manager to add the official HashiCorp Linux repository
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
+# install terraform
+sudo yum -y install terraform
+
+# verify terraform is installed
+terraform --version
+
+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
